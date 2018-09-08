c8ccd6790a14a38e45afdaaf80eb1d70c50ea389
mnem1c <dmitriytrondin@gmail.com>
2018-09-08 19:27:14 +0300

    Добавляет декоративные элементы 4. фильтр + модалка

---
+++ js/script.js
@@ -0,0 +1,49 @@
+var link = document.querySelector (".contacts-button");
+var popup = document.querySelector(".modal-write-us");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=e-mail]");
+var isStorageSupport = true;
+var storage = "";
+	try {
+	storage = localStorage.getItem("login");
+	} catch (err) {
+	isStorageSupport = false;
+	}
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+	if (storage) {
+		login.value = storage;
+		email.focus();
+	} else {
+		login.focus();
+	}
+});
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
+});
+form.addEventListener("submit", function (evt) {
+	if (!login.value || !email.value) {
+	evt.preventDefault();
+	popup.classList.remove("modal-error");
+	popup.offsetWidth = popup.offsetWidth;
+	popup.classList.add("modal-error");
+	} else {
+      if (isStorageSupport) {
+		localStorage.setItem("login", login.value);
+		}
+    }
+});
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+	if (popup.classList.contains("modal-show")) {
+	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
+	}
+}
+});
